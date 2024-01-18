c0dd064d8f69b505ac43817eaf957220b50e4de4
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-19 01:17:36 +0200

    using group by & order by

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,16 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
+-- for store in temp table
+-- CREATE TEMPORARY TABLE temp_band_fans AS
+-- SELECT
+--     origin,
+--     SUM(fans) AS nb_fans
+-- FROM
+--     metal_bands
+-- GROUP BY
+--     origin
+-- ORDER BY
+--     nb_fans
+--     DESC
+
 select origin, SUM(fans) nb_fans from `metal_bands` group by origin ORDER BY nb_fans DESC;

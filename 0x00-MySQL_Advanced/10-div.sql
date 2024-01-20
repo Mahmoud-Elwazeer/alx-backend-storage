02cebeefeca66a5f24a12366f4ded78115896a8b
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-20 04:18:29 +0200

    return ;

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -13,10 +13,11 @@ DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF (b = 0) THEN
-        RETURN (0)
+    IF (b = 0)
+    THEN
+        RETURN (0);
     ELSE
-        RETURN (a / b)
+        RETURN (a / b);
     END IF;
 END;
 

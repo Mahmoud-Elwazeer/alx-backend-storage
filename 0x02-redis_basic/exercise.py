e007c7280e836fb40423d6031a0911f6fff5eb87
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-24 19:20:55 +0200

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,15 +9,17 @@ class Cache:
     """Writing Data to Redis"""
     def __init__(self):
         """init class"""
-        self._redis: redis.Redis = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb
 
     def store(self, data: Union[str, float, bytes, int]) -> str:
         """take data argument and return key"""
-        key: str = str(uuid.uuid4())
-        if (isinstance(data, (str, bytes))):
-            self._redis.set(key, data)
-        elif (isinstance(data, (int, float))):
-            self._redis(key, str(data))
+        random_key: str = str(uuid.uuid4())
+        # if (isinstance(data, (str, bytes))):
+        #     self._redis.set(key, data)
+        # elif (isinstance(data, (int, float))):
+        #     self._redis(key, str(data))
 
-        return key
+        self._redis.set(random_key, data)
+
+        return random_key

b6e8e1d5cabe1b7ecbcedd7724dc1e9e3baa26a2
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-24 18:46:44 +0200

    redis using set in python

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""import libraries"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache():
+    """Writing Data to Redis"""
+    def __init__(self):
+        """init class"""
+        self._redis: redis.StrictRedis = redis.Redis()
+        self._redis.flushdb
+    
+    def store(self, data:[str, float, bytes, int]) -> str:
+        """take data argument and return key"""
+        self.key: str = str(uuid.uuid4())
+        self._redis.set(self.key, data)
+
+        return self.key
+
+
+

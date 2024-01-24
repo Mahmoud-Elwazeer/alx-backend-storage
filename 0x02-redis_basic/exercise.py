c0fc8d0803aeed16560de8c00f74ba6943d40d89
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-24 18:48:10 +0200

    edit style

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,13 +11,10 @@ class Cache():
         """init class"""
         self._redis: redis.StrictRedis = redis.Redis()
         self._redis.flushdb
-    
-    def store(self, data:[str, float, bytes, int]) -> str:
+
+    def store(self, data: [str, float, bytes, int]) -> str:
         """take data argument and return key"""
         self.key: str = str(uuid.uuid4())
         self._redis.set(self.key, data)
 
         return self.key
-
-
-

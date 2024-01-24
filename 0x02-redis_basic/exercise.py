ba18c29c7dbc42f81235540863fb5934c2850880
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-24 20:29:04 +0200

    return the data back to the desired format.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """import libraries"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,17 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, float, bytes, int, None]:
+        value = self._redis.get(key)
+        if value is not None:
+            if fn is not None:
+                return fn(value)
+        else:
+            None
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=str)
+    
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)

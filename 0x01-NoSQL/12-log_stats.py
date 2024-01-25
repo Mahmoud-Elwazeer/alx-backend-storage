f12fe6b758f66e8ae9edd6b36d16c51091197e52
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-25 03:27:15 +0200

    using if __name__

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,19 +2,25 @@
 """import libraries"""
 from pymongo import MongoClient
 
-client = MongoClient(host="localhost", port=27017)
-db = client.logs
-col = db.nginx
 
-all_logs = col.estimated_document_count()
-print("{} logs".format(all_logs))
+def main():
+    """main func"""
+    client = MongoClient(host="localhost", port=27017)
+    db = client.logs
+    col = db.nginx
 
-print("Methods:")
+    all_logs = col.estimated_document_count()
+    print("{} logs".format(all_logs))
 
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for i in method:
-    num_methods = col.count_documents({'method': i})
-    print('    method {}: {}'.format(i, num_methods))
+    print("Methods:")
 
-num_status = col.count_documents({'path': '/status'})
-print("{} status check".format(num_status))
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in method:
+        num_methods = col.count_documents({'method': i})
+        print('    method {}: {}'.format(i, num_methods))
+
+    num_status = col.count_documents({'path': '/status'})
+    print("{} status check".format(num_status))
+
+if __name__ == "__main__":
+    main()
